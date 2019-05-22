afd8c386f00cbdd379852e60e6b4ef4986c77fd4
fernandabassani1 <fernandabassani1@gmail.com>
2019-05-22 14:57:25 -0300

    Acrescentando um id e get&Set

--- projeto-web/src/br/com/sisvenda/model/Produto.java
+++ projeto-web/src/br/com/sisvenda/model/Produto.java
@@ -2,6 +2,7 @@ package br.com.sisvenda.model;
 
 public class Produto {
 	
+	private Integer	Id;
 	private String descricao;
 	private double vlrUnitario;
 	
@@ -21,6 +22,14 @@ public class Produto {
 
 	public void setVlrUnitario(double vlrUnitario) {
 		this.vlrUnitario = vlrUnitario;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
 	}
 
 	@Override
